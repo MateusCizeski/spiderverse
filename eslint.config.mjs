92ab0854b7e98cfd448b13f88fe91e5ad7ff16a0
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-04-07 19:48:23 -0300

    estrutura inicial do projeto

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import pluginImport from "eslint-plugin-import";
+import pluginUnusedImports from "eslint-plugin-unused-imports";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +11,28 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default [
+  ...compat.extends(["next/core-web-vitals", "prettier"], "next/typescript"),
+  {
+    plugins: {
+      import: pluginImport,
+      unusedImports: pluginUnusedImports,
+    },
+    rules: {
+      "import/no-unresolved": "error",
+      "import/order": ["warn", { groups: ["builtin", "external", "internal"] }],
 
-export default eslintConfig;
+      "unused-imports/no-unused-imports": "error",
+      "import/order": ["error", { "newlines-between": "always" }],
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
