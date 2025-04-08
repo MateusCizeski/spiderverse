4b1a54983435ec70394b79b727c50d59fec9350c
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-04-08 10:57:26 -0300

    estrutura do projeto

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    API_URL: "https://64a54c6300c3559aa9bf7245.mockapi.io",
+    DOMAIN_ORIGIN: "http://localhost:3000",
+  },
 };
 
 export default nextConfig;
