75c34f5ccda4ed8b443cca2006aa159e33167ff8
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-04-12 09:36:23 -0300

    estilazação de font e pagina de heroi.

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,6 @@
+import { spidermanFont } from "@/fonts";
 import { IHeroData } from "@/interfaces/heroes";
+import styles from "./heroesList.module.scss";
 
 interface IProps {
   heroes: IHeroData[];
@@ -7,7 +9,9 @@ interface IProps {
 export default function HeroesList({ heroes }: IProps) {
   return (
     <>
-      <h1>Personagens</h1>
+      <h1 className={`${spidermanFont.className} ${styles.title}`}>
+        Personagens
+      </h1>
       {heroes.map((hero) => (
         <p key={hero.id}>{hero.name}</p>
       ))}
