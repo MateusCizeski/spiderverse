cee454b3ebf238483aa19168068d32f87c772b6e
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-04-14 14:06:41 -0300

    estilazação das imagens

--- src/components/HeroesList/index.tsx
+++ src/components/HeroesList/index.tsx
@@ -1,6 +1,7 @@
 import { spidermanFont } from "@/fonts";
 import { IHeroData } from "@/interfaces/heroes";
 import styles from "./heroesList.module.scss";
+import HeroPicture from "../HeroPicture";
 
 interface IProps {
   heroes: IHeroData[];
@@ -12,9 +13,16 @@ export default function HeroesList({ heroes }: IProps) {
       <h1 className={`${spidermanFont.className} ${styles.title}`}>
         Personagens
       </h1>
-      {heroes.map((hero) => (
-        <p key={hero.id}>{hero.name}</p>
-      ))}
+      <section className={styles.heroes}>
+        {heroes.map((hero) => (
+          <div
+            key={hero.id}
+            className={`${styles.imageContainer} ${styles[hero.id]}`}
+          >
+            <HeroPicture hero={hero} />
+          </div>
+        ))}
+      </section>
     </>
   );
 }
