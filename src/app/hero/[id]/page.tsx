dac0050b74029581aabb79978d55c159f6e29a30
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-04-17 09:38:21 -0300

    passando informações para o componente carrossel

--- src/app/hero/[id]/page.tsx
+++ src/app/hero/[id]/page.tsx
@@ -1,9 +1,20 @@
+import Carousel from "@/components/Carousel";
+import { IHeroData } from "@/interfaces/heroes";
+
 interface IProps {
   params: {
     id: string;
   };
 }
 
-export default function Hero({ params: { id } }: IProps) {
-  return <h1>spider selecionado: {id}</h1>;
+async function getHeroesData(): Promise<{ data: IHeroData[] }> {
+  const res = await fetch(`${process.env.DOMAIN_ORIGIN}/heroes`);
+
+  return res.json();
+}
+
+export default async function Hero({ params: { id } }: IProps) {
+  const heroes = await getHeroesData();
+
+  return <Carousel heroes={heroes.data} activeId={id} />;
 }
