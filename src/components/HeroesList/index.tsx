1c3cfbfae21ba5c557f05919c35f9c3393de78b0
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2025-04-20 21:04:52 -0300

    Implementando page de details dos herois

--- src/components/HeroesList/index.tsx
+++ src/components/HeroesList/index.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+import HeroPicture from "../HeroPicture";
+
+import styles from "./heroesList.module.scss";
+
 import { spidermanFont } from "@/fonts";
 import { IHeroData } from "@/interfaces/heroes";
-import styles from "./heroesList.module.scss";
-import HeroPicture from "../HeroPicture";
-import { motion } from "framer-motion";
-import Link from "next/link";
 
 interface IProps {
   heroes: IHeroData[];
@@ -18,15 +21,14 @@ export default function HeroesList({ heroes }: IProps) {
         className={`${spidermanFont.className} ${styles.title}`}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ duration: 2, delay: 2 }}
+        transition={{ delay: 2, duration: 2 }}
       >
         Personagens
       </motion.h1>
-
       <motion.section
         className={styles.heroes}
-        initial={{ opacity: 0, y: -100 }}
-        animate={{ opacity: 1, y: 0 }}
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 2 }}
       >
         {heroes.map((hero) => (
@@ -37,7 +39,7 @@ export default function HeroesList({ heroes }: IProps) {
             whileTap={{ scale: 0.8 }}
             transition={{ duration: 0.8 }}
           >
-            <Link href={`hero/${hero.id}`}>
+            <Link href={`/hero/${hero.id}`}>
               <HeroPicture hero={hero} />
             </Link>
           </motion.div>
